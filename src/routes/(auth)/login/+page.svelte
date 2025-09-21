bb906bc5bc64672bd632e55fcd779009a7f2861b
AminBadani <ahmadaminbadani@gmail.com>
2025-09-22 06:38:07 +0700

    create register and login page

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+    <h1>Sign-In</h1>
+    <p>Belum punya akun? <a href="/register">register</a></p>
+    <form action="">
+        <div class="input">
+            <label for="email">Email</label>
+            <input type="text" placeholder="Enter your email here">
+        </div>
+        <div class="input">
+            <label for="password">Password</label>
+            <input type="text" placeholder="Enter your Password">
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</div>
