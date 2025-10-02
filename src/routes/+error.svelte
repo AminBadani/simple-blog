a47b03101923548e67c1f41760726326bb91e2bf
AminBadani <ahmadaminbadani@gmail.com>
2025-10-02 20:39:17 +0700

    create simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<div class="error-container">
+    <h1>404</h1>
+    <p>Halaman tidak ditemukan</p>
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
+
+    .error-container {
+        font-family: 'JetBrains Mono';
+        width: 98vw;
+        height: 97vh;
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+</style>
