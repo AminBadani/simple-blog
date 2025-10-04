a8b26a7f170f3f8bccdb411e5bbead46f14f7485
AminBadani <ahmadaminbadani@gmail.com>
2025-10-04 20:21:04 +0700

    redirect when already logged in and after success register

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    let { form } = $props();
+
     let userInput = $state({
         email: "testuser@email.com",
         username: "test_user",
@@ -7,32 +9,7 @@
     });
     let showPass = $state(false);
     let showConfirmPass = $state(false);
-    let message = $state('')
-
-    async function register() {
-        if (!userInput.email) return message = "Email tidak boleh kosong"
-        if (!userInput.username.trim()) return message = "Username tidak boleh kosong"
-        if (!userInput.password) return message = "Password tidak boleh kosong"
-        if (userInput.confirmPassword != userInput.password) return message = "Konfirmasi password tidak sama dengan password"
-        message = ''
-
-        const requestOptions = {
-            method: 'POST',
-            body: JSON.stringify({
-                email: userInput.email,
-                username: userInput.username,
-                password: userInput.password,
-            }),
-            headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-            }
-        }
-
-        const response = await fetch('/api/user', requestOptions);
-        const data = await response.json()
-        message = data.message;
-    }
+    let message = $state(form?.message ?? "");
 </script>
 
 <div class="container">
@@ -43,32 +20,32 @@
         <div class="message">Message: {message}</div>
     {/if}
 
-    <form onsubmit={() => register()}>
+    <form method="POST">
         <div class="input">
             <label for="email">Email</label>
             <input
+                name="email"
                 type="email"
                 placeholder="Enter your email here"
                 bind:value={userInput.email}
-                
             />
         </div>
         <div class="input">
             <label for="username">Username</label>
             <input
+                name="username"
                 type="text"
                 placeholder="Your username"
                 bind:value={userInput.username}
-                
             />
         </div>
         <div class="input">
             <label for="password">Password</label>
             <input
+                name="password"
                 type={showPass ? "text" : "password"}
                 placeholder="Enter your Password"
                 bind:value={userInput.password}
-                
             />
             <input
                 type="checkbox"
@@ -79,10 +56,10 @@
         <div class="input">
             <label for="confirm_password">Confirm Password</label>
             <input
+                name="confirm_password"
                 type={showConfirmPass ? "text" : "password"}
                 placeholder="Confirm your Password"
                 bind:value={userInput.confirmPassword}
-                
             />
             <input
                 type="checkbox"
