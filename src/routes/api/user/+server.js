2e66e92a8d1a228654a975c86d93fda01a59641b
AminBadani <ahmadaminbadani@gmail.com>
2025-10-04 20:21:55 +0700

    check existed email and return new user

--- src/routes/api/user/+server.js
+++ src/routes/api/user/+server.js
@@ -11,16 +11,21 @@ export async function GET() {
 
 export async function POST ({ request }) {
     const { email, username, password, bio } = await request.json()
+
+    const checkUser = await supabase.from('users').select().eq('email', email);
+    if (checkUser.data?.length) return json({ message: 'Email sudah ada yang menggunakan' })
+
     const response = await supabase.from('users').insert({
         email: email,
         username: username, 
         password: password,
         bio: bio
-    })
+    }).select()
     
     if (response.error) return json(response.error, { status: 409 })
 
     return json({
+        user: response.data[0],
         message: "User berhasil dibuat" 
     });
 }
